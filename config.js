914d71fb78d66b77307f6a602624d8a36a07010f
christian <69432633+Aegians@users.noreply.github.com>
2022-12-06 23:35:17 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+exports.TOKEN = ''; //https://discord.com/developers/applications
+
+exports.DBL_API_KEY = ''; //https://top.gg
+
+exports.PREFIX = '';
+
+exports.GOOGLE_API_KEY = ''; // https://console.cloud.google.com/apis/library/youtube.googleapis.com
+
+exports.GENIUS_API_KEY = ''; // https://genius.com/developers
+
+exports.yandex_API = ''; // https://yandex.com/dev/predictor/keys/get/
+
+exports.news_API = ''; // https://newsapi.org/
+
+exports.giphy_API = ''; // https://developers.giphy.com/
+
+exports.AME_API = ''; // https://api.amethyste.moe
+
+exports.blague_API = ''; // https://blague.xyz/
